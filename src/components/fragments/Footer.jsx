e975dbc4817e27ee42fafdcf7afe3628c067fd69
Abdul Ghofur <119642022+Ra-Ghofur@users.noreply.github.com>
2025-09-14 14:50:43 +0700

    Update Footer.jsx

--- src/components/fragments/Footer.jsx
+++ src/components/fragments/Footer.jsx
@@ -10,8 +10,8 @@ const Footer = () => {
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-sm text-white/90">
 
                     {/* Kiri - Logo & Slogan */}
-                    <div className="flex items-center gap-3 mb-2">
                         <div className="flex flex-col items-center md:items-start text-center md:text-left>
+                            <div className="flex items-center gap-3 mb-2">
                             <img src={Logo} alt="Logo" className="h-10 w-10 drop-shadow-md" />
                             <span className="text-xl font-bold drop-shadow-sm">JAGAT</span>
                         </div>
