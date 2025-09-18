e3c8291eea417cfb5f3270403d2b29eac1ca26cf
rafi andrea <rafiandrealesmana@gmail.com>
2025-09-18 12:13:17 +0700

    fix footer icon

--- src/components/fragments/Footer.jsx
+++ src/components/fragments/Footer.jsx
@@ -1,6 +1,6 @@
 import { FaGithub } from "react-icons/fa";
-import { FaInstagram, FaFacebook } from "react-icons/fa6";
-{/* import Logo from '../../../public/assets/image/logo.png' */}
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
     return (
@@ -11,8 +11,8 @@ const Footer = () => {
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-sm text-white/90">
 
                     {/* Kiri - Logo & Slogan */}
-                        <div className="flex flex-col items-center md:items-start text-center md:text-left">
-                            <div className="flex items-center gap-3 mb-2">
+                    <div className="flex flex-col items-center md:items-start text-center md:text-left">
+                        <div className="flex items-center gap-3 mb-2">
                             <img src="/assets/image/logo.png" alt="Logo" className="h-10 w-10 drop-shadow-md" />
                             <span className="text-xl font-bold drop-shadow-sm">JAGAT</span>
                         </div>
@@ -29,32 +29,31 @@ const Footer = () => {
                     {/* Kanan - Ikuti Kami */}
                     <div className="flex flex-col items-center md:items-end text-center md:text-right mx-auto md:mx-0">
                         <p className="font-semibold mb-1">Ikuti kami</p>
-                        <a
-                            href="https://jagung-sehat.vercel.app/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            {/*className="text-white hover:text-white/80 mt-1"*/}
-                        >
-                            <FaGithub size={22} color="black"/>
-                        </a>
-                        {/* Link Instagram */}
-                      <a
-                        href="https://instagram.com/ra.ghofur"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        {/* className="text-white hover:text-white/80 mt-1"*/}
-                      >
-                        <FaInstagram size={22} color="white"/>
-                      </a>
-                         {/* Link Facebook */}
-                      <a
-                        href="https://facebook.com/abdulghofur"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                          {/* className="text-white hover:text-white/80 mt-1" */}
-                      >
-                        <FaFacebook size={22} color="black"/>
-                      </a>
+                        <div className="flex items-center gap-4">
+                            <a
+                                href="https://jagung-sehat.vercel.app/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FaGithub size={22} color="white" />
+                            </a>
+                            {/* Link Instagram */}
+                            <a
+                                href="https://instagram.com/ra.ghofur"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FaInstagram size={22} color="white" />
+                            </a>
+                            {/* Link Facebook */}
+                            <a
+                                href="https://facebook.com/abdulghofur"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FaFacebook size={22} color="white" />
+                            </a>
+                        </div>
                     </div>
                 </div>
 
@@ -68,9 +67,9 @@ const Footer = () => {
                     <span>© 2025 Dinas Pertanian TPHP KAB. BANGKALAN</span>
                 </div> */}
                 <div className="text-center text-sm text-white/80 flex flex-col md:flex-row md:justify-center md:space-x-4 space-y-2 md:space-y-0">
-                      <span className="hover:underline">Terms & Conditions</span>
-                      <span className="hover:underline">Privacy Policy</span>
-                      <span>© 2025 Dinas Pertanian TPHP KAB. BANGKALAN</span>
+                    <span className="hover:underline">Terms & Conditions</span>
+                    <span className="hover:underline">Privacy Policy</span>
+                    <span>© 2025 Dinas Pertanian TPHP KAB. BANGKALAN</span>
                 </div>
 
             </div>
